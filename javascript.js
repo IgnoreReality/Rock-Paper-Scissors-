d11dcdd5f3bd4e3c6633758dfc92425184ab03a5
IgnoreReality <manuelfframires@gmail.com>
2024-06-29 20:48:10 +0100

    adding plan.txt and machine choice to game in js file

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 console.log("Hello World")
+
+function getComputerChoice(){
+
+    let num = Math.random()
+    let hand = ""
+
+    if (num<(1/3)) {
+        hand="Rock";
+
+    } else if (num>=(1/3) && num<(2/3)) {
+        hand="Paper";
+
+    } else {
+        hand="Scissors";
+    }
+    console.log(num)
+
+return hand;
+
+}
+console.log(getComputerChoice())
